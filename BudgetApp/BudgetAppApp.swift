a8af1e550928116cba1f8ead69201e71b4c470d8
bee <3172736837@qq.com>
2025-03-17 23:44:57 +0800

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/3/17.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetAppTests/BudgetAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppTests.swift
+//  BudgetAppTests
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/3/17.
+//
+
+import Testing
+@testable import BudgetApp
+
+struct BudgetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
